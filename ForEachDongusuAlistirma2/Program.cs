2cdf1ab97f374c30f8dd95c8f8d3ee55b860c654
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 21:22:19 +0300

    C# ForEach Dongusu Alistirma 2 Yapıldı.

--- ForEachDongusuAlistirma2/Program.cs
+++ ForEachDongusuAlistirma2/Program.cs
@@ -10,6 +10,20 @@ namespace ForEachDongusuAlistirma2
     {
         static void Main(string[] args)
         {
+            string[] siparis = { "Telefon", "Kulaklık", "Laptop", "Bilgisayar", "Süpürge", "PowerBank" };
+
+            int sayac = 1;
+            int toplam = 0;
+            foreach(var item in  siparis)
+            {
+                Console.WriteLine("{0}. sipariş Adı : {1}", sayac, item);
+                sayac++;
+               
+            }
+            toplam = siparis.Length;
+            Console.WriteLine("Toplam Sipariş : " +  toplam);
+
+            
         }
     }
 }
