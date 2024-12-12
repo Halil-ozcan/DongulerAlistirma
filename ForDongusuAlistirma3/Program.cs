c3afaf84b94aab6714cc7ce70f9d553d07c17f86
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 20:59:03 +0300

    C# For Dongusu Alistırma 3 Yapıldı

--- ForDongusuAlistirma3/Program.cs
+++ ForDongusuAlistirma3/Program.cs
@@ -10,6 +10,46 @@ namespace ForDongusuAlistirma3
     {
         static void Main(string[] args)
         {
+            int[] OgrenciNot =  { 50, 85, 45, 90, 70, 60, 55, 65, 100, 40 };
+
+            int ortalama = 0;
+            int toplam = 0;
+            int ortalamaAltındaKalan = 0;
+
+            for(int i = 0; i < OgrenciNot.Length; i++)
+            {
+                int not = OgrenciNot[i];
+                Console.WriteLine(not);
+
+                toplam = toplam + not;
+            }
+
+            Console.WriteLine("----------------------------------------------------");
+
+            ortalama = toplam / OgrenciNot.Length;
+
+            for(int i = 0; i<OgrenciNot.Length; i++)
+            {
+                if (OgrenciNot[i] <  ortalama)
+                {
+                    ortalamaAltındaKalan++;
+                }
+            }
+
+            Console.WriteLine("Notların Ortalaması: " + ortalama);
+            Console.WriteLine("Ortalamanın Altında Kalan Öğrenci Sayısı: " + ortalamaAltındaKalan);
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            Console.WriteLine("Ortalamanın Altında Kalan Öğrencilerin Notları:");
+
+            for(int i = 0;i < OgrenciNot.Length;i++)
+            {
+                if (OgrenciNot[i] < ortalama)
+                {
+                    Console.WriteLine("{0}. Öğrenci Notu : {1}", i, OgrenciNot[i]);
+                }
+            }
+
         }
     }
 }
