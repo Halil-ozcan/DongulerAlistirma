91cecbc14fe0d9c3f750a4ef76fdd8f5707b3d1d
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 19:42:08 +0300

    C# For Dongusu Alistirma 2 Yapıldı.

--- ForDongusuAlistirma2/Program.cs
+++ ForDongusuAlistirma2/Program.cs
@@ -10,6 +10,34 @@ namespace ForDongusuAlistirma2
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int[] calisan = new int[10];
+
+
+            int maxMaas = calisan[0];
+            int maxIndex = 0;
+            for (int i = 0;  i < calisan.Length; i++)
+            {
+                calisan[i] = rnd.Next(18000,50000);
+                int maas = calisan[i];
+                Console.WriteLine("{0}. Çalışanın Maaşı : {1}", i+1, maas);
+                if (calisan[i] > maxMaas)
+                {
+                    maxMaas = calisan[i];
+                    maxIndex = i;
+                }
+
+                Console.WriteLine("-----------------------------------------");
+            }
+            Console.WriteLine("{0}. Çalışan Kişi En Yüksek Maaşı Alıyor: {1}", maxIndex + 1, maxMaas);
+
+
+
+
+
+
+
+
         }
     }
 }
