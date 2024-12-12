23c7a375258f010f1350b35e6cf683c5a2f122d9
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 21:11:48 +0300

    C# ForEach Dongusu 1 Yapıldı.

--- ForEachAlistirma1/Program.cs
+++ ForEachAlistirma1/Program.cs
@@ -10,6 +10,13 @@ namespace ForEachAlistirma1
     {
         static void Main(string[] args)
         {
+            string[] urunler = { "Yumurta", "Peynir", "Süt", "Sucuk", "Zeytin", "Salam" };
+            int sayac = 1;
+            foreach(var item in urunler)
+            {
+                Console.WriteLine("{0}. Ürün Adı : {1}", sayac, item);
+                sayac++;
+            }
         }
     }
 }
