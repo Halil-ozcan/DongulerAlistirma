6b87ab29b494dde970fa4fc13d237460a56b734f
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 18:52:03 +0300

    C# For Alistirma 1 Yapıldı

--- DongulerAlistirma/Program.cs
+++ DongulerAlistirma/Program.cs
@@ -10,6 +10,29 @@ namespace DongulerAlistirma
     {
         static void Main(string[] args)
         {
+            int[] urunFiyat = new int[5];
+            urunFiyat[0] = 100;
+            urunFiyat[1] = 300;
+            urunFiyat[2] = 500;
+            urunFiyat[3] = 700;
+            urunFiyat[4] = 1000;
+            int toplam = 0;
+
+            Console.WriteLine("Fatura Yazdırma Örneği");
+            for (int i = 0; i<urunFiyat.Length; i++)
+            {
+                int urun = urunFiyat[i];
+                Console.WriteLine("{0} . ürünün Fiyati : {1}", i , urun + " TL ");
+
+                Console.WriteLine("--------------------------------------------");
+
+                toplam = toplam + urun;
+                
+            }
+            Console.WriteLine("Toplam Tutarınız : " + toplam + " TL ");
+
+
+
         }
     }
 }
