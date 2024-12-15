c70da6c599f81553320139276faf92c65f5362c8
Halil Özcan <halil_ozcann@outlook.com>
2024-12-15 15:36:59 +0300

    C# While Dongusu Alistirma 2 Yapıldı

--- DongulerAlistirma2/Program.cs
+++ DongulerAlistirma2/Program.cs
@@ -10,6 +10,37 @@ namespace DongulerAlistirma2
     {
         static void Main(string[] args)
         {
+
+            string kullaniciadi = "admin";
+            string sifre = "test";
+
+            int hak = 3;
+
+            while(hak > 0)
+            {
+                Console.WriteLine("Kullanici Adi : ");
+                string GelenKullaniciAdi = Console.ReadLine();
+
+                Console.WriteLine("Sifre : ");
+                string GelenSifre = Console.ReadLine();
+
+                if (kullaniciadi == GelenKullaniciAdi && sifre == GelenSifre)
+                {
+                    Console.WriteLine("Giriş Başarılı");
+                    break;
+                }
+                else
+                {
+                    hak--;
+                    Console.WriteLine("Giriş Başarısız Kalan Hakkınız : " + hak);
+                }
+                if(hak == 0)
+                {
+                    Console.WriteLine("Giriş Hakkınız Doldu");
+                }
+            }
+            
+           
         }
     }
 }
